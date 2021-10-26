80629ada269bcd612a788e3f2c169c4b1caaf724
3LucasZ <lucas.j.zheng@gmail.com>
2021-10-26 16:51:49 -0700

    intelij anoying refactor AGAIN

---
+++ UsacoJava/src/WhereAmI.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class WhereAmI {
+    public static void main(String[] args) throws IOException {
+        /*BufferedReader f = new BufferedReader(new FileReader("whereami.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("whereami.out")));
+        int k = 1;
+        int n = Integer.parseInt(f.readLine());
+        String colors = f.readLine();
+        out.println(colors);
+
+         */
+        System.out.println("Hello");
+    }
+}
--- UsacoJava/src/mmm/Template.java
+++ UsacoJava/src/mmm/Template.java
@@ -1,7 +1,9 @@
+package mmm;
+
 import java.io.*;
 import java.util.*;
 
-class Template {
+public class Template {
     public static void main(String[] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader("template.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("template.out")));
