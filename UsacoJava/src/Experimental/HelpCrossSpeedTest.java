fa46ec03f4c503a056910d92c0cc888f49623632
3LucasZ <lucas.zheng@warriorlife.net>
2021-12-13 10:14:54 -0800

    new module

---
+++ UsacoJava/src/Experimental/HelpCrossSpeedTest.java
@@ -0,0 +1,83 @@
+package Experimental;
+
+import java.util.*;
+import java.io.*;
+/*
+Java solution to:
+USACO 2017 February Contest, Silver
+Problem 1. Why Did the Cow Cross the Road
+from online. My solution is extremely similar but "Too slow"
+Turns out this is 500ms faster.
+My guesses for issue: slow IO, parallel array instead of custom object, additional random code that was not really needed
+ */
+public class HelpCrossSpeedTest {
+    public static void main (String[] args) throws IOException {
+        Kattio io = new Kattio("helpcross");
+        int numChickens = io.nextInt();
+        int numCows = io.nextInt();
+        int[] chicken = new int[numChickens]; // where the chicken are located
+        int[] earliest = new int[numCows];
+        int[] latest = new int[numCows];
+        boolean[] marked = new boolean[numCows];
+
+        for (int x = 0; x < numChickens; x++) {
+            chicken[x] = io.nextInt();
+        }
+        for (int x = 0; x < numCows; x++){
+            earliest[x] = io.nextInt();
+            latest[x] = io.nextInt();
+        }
+        Arrays.sort(chicken);
+
+        int count = 0;
+        for (int x = 0; x < numChickens; x++) { // loop through each chicken
+            int smallest = -1;
+            // find the cow that ends the soonest that the chicken is in its range
+            for (int y = 0; y < numCows; y++) {
+                // the cow has not crossed the road yet and the
+                // chicken's position is able to help the cow cross
+                if (! marked[y] && chicken[x] >= earliest[y] && chicken[x] <= latest[y]) {
+                    if (smallest < 0 || latest[y] < latest[smallest]) {
+                        smallest = y;
+                    }
+                }
+            }
+            if (smallest != -1) {
+                marked[smallest] = true;
+                count++;
+            }
+        }
+        io.println(count);
+        io.close();
+    }
+    static class Kattio extends PrintWriter {
+        private BufferedReader r;
+        private StringTokenizer st;
+
+        // standard input
+        public Kattio() { this(System.in, System.out); }
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+        // USACO-style file input
+        public Kattio(String problemName) throws IOException {
+            super(new FileWriter(problemName + ".out"));
+            r = new BufferedReader(new FileReader(problemName + ".in"));
+        }
+
+        // returns null if no more input
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens())
+                    st = new StringTokenizer(r.readLine());
+                return st.nextToken();
+            } catch (Exception e) { }
+            return null;
+        }
+
+        public int nextInt() { return Integer.parseInt(next()); }
+        public double nextDouble() { return Double.parseDouble(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
---
+++ UsacoJava/src/UsacoGuideSilver/PrefixSums/HoofPaperScissors.java
@@ -0,0 +1,79 @@
+package UsacoGuideSilver.PrefixSums;
+
+import java.io.*;
+import java.util.*;
+/*
+USACO 2017 January Contest, Silver
+Problem 2. Hoof, Paper, Scissors
+USACO Guide Silver - Prefix Sums - Easy
+ */
+public class HoofPaperScissors {
+    //io
+    static boolean submission = true;
+    static PrintWriter out;
+    static BufferedReader f;
+    //param
+    static int n;
+    static int[] HSum;
+    static int[] PSum;
+    static int[] SSum;
+    public static void main(String[] args) throws IOException {
+        //io
+        if (submission) {
+            f = new BufferedReader(new FileReader("hps.in"));
+            out = new PrintWriter(new BufferedWriter(new FileWriter("hps.out")));
+        }
+        else {
+            f = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+        }
+        //parse input
+        n = Integer.parseInt(f.readLine());
+        HSum = new int[n+1];
+        PSum = new int[n+1];
+        SSum = new int[n+1];
+        for (int i=1;i<n+1;i++) {
+            HSum[i]=HSum[i-1];
+            PSum[i]=PSum[i-1];
+            SSum[i]=SSum[i-1];
+            char fj = f.readLine().charAt(0);
+            if (fj=='H'){
+                PSum[i]++;
+            }
+            else if (fj=='P'){
+                SSum[i]++;
+            }
+            else {
+                HSum[i]++;
+            }
+        }
+        //out.println(Arrays.toString(HSum));
+        //out.println(Arrays.toString(PSum));
+        //out.println(Arrays.toString(SSum));
+        int max=0;
+        for (int i=0;i<n;i++) {
+            int HBefore = HSum[i];
+            int HAfter = HSum[n] - HSum[i];
+            int PBefore = PSum[i];
+            int PAfter = PSum[n] - PSum[i];
+            int SBefore = SSum[i];
+            int SAfter = SSum[n] - SSum[i];
+            max = Math.max(HBefore + HAfter, max);
+            max = Math.max(HBefore + PAfter, max);
+            max = Math.max(HBefore + SAfter, max);
+
+            max = Math.max(PBefore + HAfter, max);
+            max = Math.max(PBefore + PAfter, max);
+            max = Math.max(PBefore + SAfter, max);
+
+            max = Math.max(SBefore + HAfter, max);
+            max = Math.max(SBefore + PAfter, max);
+            max = Math.max(SBefore + SAfter, max);
+        }
+        //logic
+        //turn in answer
+        out.println(max);
+        out.close();
+        f.close();
+    }
+}
