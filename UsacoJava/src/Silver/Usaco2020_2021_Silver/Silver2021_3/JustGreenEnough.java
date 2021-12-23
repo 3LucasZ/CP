b0a1624a1ed074e96f61fa78e73a451c5530366d
3LucasZ <lucas.zheng@warriorlife.net>
2021-12-23 00:29:42 -0800

    Bovine Shuffle, Teleportation

--- UsacoJava/src/Silver/Usaco2020_2021_Silver/Silver2021_3/JustGreenEnough.java
+++ UsacoJava/src/Silver/Usaco2020_2021_Silver/Silver2021_3/JustGreenEnough.java
@@ -1,3 +1,5 @@
+package Silver.Usaco2020_2021_Silver.Silver2021_3;
+
 import java.io.*;
 import java.util.*;
 /*
@@ -5,7 +7,7 @@ USACO 2021 February Contest, Silver
 Problem 3. Just Green Enough
 Solving Session - Normal
 Concepts: Complementary counting, 2 pointers, prefix sums
-Problem Statement: finding all rectangles of 2d array that contain only a certain character
+The main problem: finding all rectangles of 2d array that contain only a certain character
  */
 public class JustGreenEnough {
     //io
@@ -30,33 +32,42 @@ public class JustGreenEnough {
         sumsAtMost = new int[N][N+1];
         for (int r=0;r<N;r++) {
             for (int c=0;c<N;c++) {
+                //0 for good: 100 and greater
                 sumsBelow[r][c+1] = sumsBelow[r][c] + (pasture[r][c] < 100 ? 1:0);
+                //0 for good: 101 and greater
                 sumsAtMost[r][c+1] = sumsAtMost[r][c] + (pasture[r][c] <= 100 ? 1 : 0);
             }
         }
         long answer = 0;
-        printArr(pasture);
-        printArr(sumsBelow);
-        printArr(sumsAtMost);
-        for (int x1=0;x1<N;x1++) {
-            for (int x2=x1+1;x2<=N;x2++) {
-                int y1 = -1;
-                int y2 = -1;
-                for (int y0=0;y0<N;y0++) {
-                    while (y1 < N && (y1 < y0 || sumsAtMost[y1][x2] - sumsAtMost[y1][x1] == 0)) {
-                        y1++;
+//        printArr(pasture);
+//        printArr(sumsBelow);
+//        printArr(sumsAtMost);
+        //logic
+        for (int c1=0;c1<=N;c1++) {
+            for (int c2=c1+1;c2<=N;c2++) {
+                long belowRun = 0;
+                long mostRun = 0;
+                for (int r=0;r<N;r++) {
+                    if (sumsBelow[r][c2]-sumsBelow[r][c1]==0) {
+                        belowRun++;
                     }
-                    while (y2 < N && (y2 < y0 || sumsBelow[y2][x2] - sumsBelow[y2][x1] == 0)) {
-                        y2++;
+                    else {
+                        belowRun=0;
                     }
-                    answer += y2 - y1;
+                    if (sumsAtMost[r][c2]-sumsAtMost[r][c1]==0) {
+                        mostRun++;
+                    }
+                    else {
+                        mostRun = 0;
+                    }
+                    answer += belowRun;
+                    answer -= mostRun;
                 }
             }
         }
         //turn in answer
         out.println(answer);
         out.close();
-        f.close();
     }
     public static void printArr(int[][] arr) {
         for (int i=0;i<arr.length;i++) {
