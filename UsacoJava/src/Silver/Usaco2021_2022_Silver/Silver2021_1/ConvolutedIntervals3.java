8881ddba36fc4b4f8338d2d5c469ffa2254e6da5
3LucasZ <lucas.j.zheng@gmail.com>
2021-12-24 22:50:38 -0800

    Update ConvolutedIntervals3.java

--- UsacoJava/src/Silver/Usaco2021_2022_Silver/Silver2021_1/ConvolutedIntervals3.java
+++ UsacoJava/src/Silver/Usaco2021_2022_Silver/Silver2021_1/ConvolutedIntervals3.java
@@ -14,12 +14,15 @@ public class ConvolutedIntervals3 {
     static int[] start;
     static int[] end;
     static int[] preSum;
+    static int[] ans;
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        start = new int[M];
-        end = new int[M];
+        start = new int[M+1];
+        end = new int[M+1];
+        preSum = new int[2*M+1];
+        ans = new int[2*M+1];
         for (int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
@@ -27,5 +30,16 @@ public class ConvolutedIntervals3 {
             start[u]++;
             end[v]++;
         }
+        for (int i=0;i<=M;i++) {
+            for (int j=0;j<=M;j++) {
+                preSum[i+j]+=start[i]*start[j];
+                preSum[i+j-1]-=end[i]*end[j];
+            }
+        }
+        ans[0] = preSum[0];
+        for (int i=1;i<=2*M;i++) {
+            ans[i]=ans[i-1]+preSum[i];
+        }
+        for (int i=0;i<=2*M;i++)
     }
 }
