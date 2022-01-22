d064a56c50552779913ad39febd69c616df9eff2
3LucasZ <lucas.zheng@warriorlife.net>
2022-01-21 20:35:41 -0800

    Update FarmPainting.java

--- UsacoJava/src/FarmPainting.java
+++ UsacoJava/src/FarmPainting.java
@@ -1,10 +1,6 @@
-import org.w3c.dom.css.Rect;
-
 import java.io.*;
 import java.util.*;
-/*
 
- */
 public class FarmPainting {
     //io
     static boolean submission = true;
@@ -12,11 +8,8 @@ public class FarmPainting {
     static BufferedReader br;
     //param
     static int N;
-    static Rectangle[] rectangles;
-    static TreeSet<Rectangle> active = new TreeSet<Rectangle>((a, b)->{
-        return a.y1-b.y1;
-    });
-    static int ans;
+    static Point[] points;
+    static TreeSet<Point> active = new TreeSet<Point>((a,b)->a.y-b.y);
     public static void main(String[] args) throws IOException {
         //io
         if (submission) {
@@ -29,56 +22,53 @@ public class FarmPainting {
         }
         //parse input
         N = Integer.parseInt(br.readLine());
-        rectangles = new Rectangle[N];
+        points = new Point[2*N];
         for (int i=0;i<N;i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            rectangles[i] = new Rectangle(x1,y1,x2,y2);
+            Point u = new Point(x1,y1,true);
+            Point v = new Point(x2,y2,false);
+            u.other = v;
+            v.other = u;
+            points[2*i] = u;
+            points[2*i+1] = v;
         }
+        //sort
+        Arrays.sort(points, (a,b)->a.x-b.x);
         //logic
-        Arrays.sort(rectangles,(a,b)->{
-            return a.x1-b.x1;
-        });
-        //out.println(Arrays.toString(rectangles));
-        for (int i=0;i<N;i++) {
-            Rectangle lower = active.lower(rectangles[i]);
-            if (lower != null){
-                if (lower.contains(rectangles[i]));
-                else if (rectangles[i].contains(lower)) {
-                    active.add(rectangles[i]);
-                    active.remove(lower);
-                }
-                else {
-                    active.add(rectangles[i]);
+        int ans = 0;
+        for (int i=0;i<2*N;i++) {
+            if (points[i].isBottom) {
+                if (active.ceiling(points[i])==null || active.ceiling(points[i]).isBottom) {
+                    ans ++;
+                    active.add(points[i]);
+                    active.add(points[i].other);
                 }
             }
             else {
-                active.add(rectangles[i]);
+                active.remove(points[i]);
+                active.remove(points[i].other);
             }
         }
-        out.println(active.size());
+        //turn in answer
+        out.println(ans);
         out.close();
     }
-    private static class Rectangle {
-        //true -> begin point | false -> end point
-        int x1;
-        int y1;
-        int x2;
-        int y2;
-        public Rectangle(int xx1, int yy1, int xx2, int yy2){
-            x1=xx1;
-            y1=yy1;
-            x2=xx2;
-            y2=yy2;
-        }
-        public boolean contains(Rectangle other){
-            return x1<=other.x1 && y1<=other.y1 && x2>=other.x2 && y2 >= other.y2;
+    private static class Point {
+        int x;
+        int y;
+        boolean isBottom;
+        Point other;
+        public Point(int x, int y, boolean isBottom){
+            this.x=x;
+            this.y=y;
+            this.isBottom = isBottom;
         }
         public String toString(){
-            return "[["+x1+", "+y1+"], ["+x2+", "+y2+"]]";
+            return "["+x+", "+y+"]";
         }
     }
 }
