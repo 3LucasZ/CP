ef0f224a27b84e7842649293bb3a2f8328d44eb0
3LucasZ <lucas.zheng@warriorlife.net>
2022-02-14 19:49:41 -0800

    Google Kickstart Prep Templates

---
+++ UsacoJava/src/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    //io
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        //parse input
+        //logic
+        //turn in answer
+        out.println();
+        out.close();
+    }
+}
