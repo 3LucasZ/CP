cd3633fa6d8b1a7e87e1e3bece9972562f811272
3LucasZ <lucas.zheng@warriorlife.net>
2022-02-22 18:58:23 -0800

    MOdernArt2

--- UsacoJava/src/Misc/Experimental/PriorityQueueTesting.java
+++ UsacoJava/src/Misc/Experimental/PriorityQueueTesting.java
@@ -7,8 +7,23 @@ public class PriorityQueueTesting {
         PriorityQueue<Integer> active = new PriorityQueue<>();
         active.add(1);
         active.add(1);
-        active.add(1);
+        active.add(8);
+        active.add(33);
         System.out.println(active);
 
+        PriorityQueue<Object> active2 = new PriorityQueue<>((a,b)->a.data-b.data);
+        active2.add(new Object(1));
+        active2.add(new Object(3));
+        active2.add(new Object(5));
+        System.out.println(active2);
+    }
+    private static class Object {
+        int data;
+        public Object(int d){
+            data=d;
+        }
+        public String toString(){
+            return ""+data;
+        }
     }
 }
