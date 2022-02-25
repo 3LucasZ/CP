3bdee3668c5798b524f393130846e782c4a2ca3f
3LucasZ <lucas.zheng@warriorlife.net>
2022-02-24 21:25:22 -0800

    Ew problems :|

---
+++ UsacoJava/src/Codeforces/Div2Round769/StrangeTest.java
@@ -0,0 +1,26 @@
+package Codeforces.Div2Round769;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class StrangeTest {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int i=0;i<T;i++) solve();
+        out.close();
+    }
+    public static void solve() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int minOps = Integer.MAX_VALUE;
+        for (int AA=A;AA<B;AA++){
+           // minOps = Math.min(minOps, AA + )
+        }
+    }
+}
