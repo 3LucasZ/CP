23559094a1eab1252dca818656f6010165cb962b
3LucasZ <lucas.zheng@warriorlife.net>
2022-03-03 20:01:50 -0800

    COWHOPSCOTCH2, PREFIX, ZEROSUM, MITM

---
+++ UsacoJava/src/MeetInTheMiddle.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class MeetInTheMiddle {
+    //io
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static boolean debug = true;
+
+    //param
+    static int N;
+    static int X;
+    static long[] arr;
+    //static HashSet<>
+
+    public static void main(String[] args) throws IOException{
+        //parse
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        arr = new long[N];
+
+    }
+}
---
+++ UsacoJava/src/zerosum.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+/*
+LANG: JAVA
+TASK: zerosum
+ */
+public class zerosum {
+    //io
+    static boolean submission = false;
+    static PrintWriter out;
+    static BufferedReader br;
+    //param
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        //io
+        if (submission) {
+            br = new BufferedReader(new FileReader("zerosum.in"));
+            out = new PrintWriter(new BufferedWriter(new FileWriter("zerosum.out")));
+        } else {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+        }
+        //parse input
+        N = Integer.parseInt(br.readLine());
+        int[] temp = new int[2 * N];
+        for (int i = 0; i < 2 * N; i += 2) {
+            temp[i] = i;
+            //temp[i+1]=
+        }
+        //logic
+        for (int i = 0; i < Math.pow(3, N - 1); i++) {
+
+        }
+        //turn in answer
+        out.println();
+        out.close();
+    }
+}
