aa0c9f0d66af5e56fdfb82cd160a55950c45a306
3LucasZ <lucas.zheng@warriorlife.net>
2022-03-11 19:46:52 -0800

    Update game248.java
    easy 248 game

--- UsacoJava/src/game248.java
+++ UsacoJava/src/game248.java
@@ -1,13 +1,17 @@
 import java.io.*;
+import java.util.*;
 
 public class game248 {
     //io
     static boolean submission = false;
     static PrintWriter out;
     static BufferedReader br;
-    static boolean debug=true;
+    static boolean debug = false;
+
     //param
     static int N;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         //io
         if (submission) {
@@ -18,11 +22,33 @@ public class game248 {
             br = new BufferedReader(new InputStreamReader(System.in));
             out = new PrintWriter(System.out);
         }
+
         //parse input
         N = Integer.parseInt(br.readLine());
-        //logic
+        //dp[start][extend]
+        int[][] dp = new int[N][N];
+        for (int i=0;i<N;i++){
+            dp[i][0] = Integer.parseInt(br.readLine());
+        }
+
+        //logic: range dp
+        for (int extend=1;extend<=N-1;extend++){
+            for (int start=0;start<=N-1-extend;start++){
+                for (int e1=0;e1<=extend-1;e1++){
+                    int e2=extend-e1-1;
+                    if (dp[start][e1]==dp[start+e1+1][e2]&&dp[start][e1]!=0){
+                        dp[start][extend]=dp[start][e1]+1;
+                        break;
+                    }
+                }
+            }
+        }
+        if (debug) for (int extend=0;extend<N;extend++) System.out.println(Arrays.toString(dp[extend]));
+
         //turn in answer
-        out.println();
+        int ans = 0;
+        for (int r=0;r<N;r++) for (int c=0;c<N;c++) ans = Math.max(ans, dp[r][c]);
+        out.println(ans);
         out.close();
     }
 }
