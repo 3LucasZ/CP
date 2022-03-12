65d450cbac5528581caeba3557ff1c18e7f126c8
3LucasZ <lucas.zheng@warriorlife.net>
2022-03-11 19:46:39 -0800

    Update Cereal2.java
    2 hard fail

--- UsacoJava/src/Cereal2.java
+++ UsacoJava/src/Cereal2.java
@@ -7,7 +7,8 @@ public class Cereal2 {
     //param
     static int N;
     static int M;
-    static ArrayList<Edge>[] graph;
+    static ArrayList<Edge>[] inEdges;
+    static ArrayList<Edge>[] outEdges;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -15,23 +16,28 @@ public class Cereal2 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[M+1];
+        inEdges = new ArrayList[M+1];
+        outEdges = new ArrayList[M+1];
         visited = new boolean[M+1];
+
         for (int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            //graph[u].add(new Edge());
-            //graph[v].add(u);
+            outEdges[u].add(new Edge(v, i));
+            inEdges[v].add(new Edge(u, i));
         }
+
         //dfs and connected components
         for (int i=1;i<=M;i++) {
             if (!visited[i]) dfs(i);
         }
     }
+
     public static void dfs(int node){
 
     }
+
     private static class Edge {
         int v;
         int id;
