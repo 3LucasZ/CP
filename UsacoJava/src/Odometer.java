e5055c6d0b0e582f38538987326c018357f68a75
3LucasZ <lucas.zheng@warriorlife.net>
2022-03-13 17:51:27 -0700

    haircut + fenwick trees

--- UsacoJava/src/Odometer.java
+++ UsacoJava/src/Odometer.java
@@ -4,66 +4,81 @@ import java.util.*;
 public class Odometer {
     //io
     static boolean submission = false;
+    static boolean debug = true;
     static PrintWriter out;
     static BufferedReader br;
+
     //param
-    static int[] S;
-    static int[] E;
-    //choose
-    static int MAX = 19;
-    static int[][] choose;
+    static int A;
+    static int[] X;
+    static int B;
+    static int[] Y;
+
     public static void main(String[] args) throws IOException {
         //io
         if (submission) {
             br = new BufferedReader(new FileReader(".in"));
             out = new PrintWriter(new BufferedWriter(new FileWriter(".out")));
-        }
-        else {
+        } else {
             br = new BufferedReader(new InputStreamReader(System.in));
             out = new PrintWriter(System.out);
         }
-        //choose test
-        choose = new int[MAX][MAX];
-        fillChoose();
-        //for (int i=0;i<MAX;i++) out.println(Arrays.toString(choose[i]));
 
-        //parse input
+        //parse
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String Sstr = st.nextToken();
-        S = new int[Sstr.length()];
-        String Estr = st.nextToken();
-        E = new int[Estr.length()];
-        for (int i=0;i<Sstr.length();i++) {
-            S[i]=Sstr.charAt(i)-48;
+        String str1 = st.nextToken();
+        A = str1.length();
+        String str2 = st.nextToken();
+        B = str2.length();
+        X = new int[A];
+        Y = new int[B];
+        for (int i=0;i<A;i++){
+            X[i] = str1.charAt(i)-48;
         }
-        for (int i=0;i<Estr.length();i++) {
-            E[i]=Estr.charAt(i)-48;
+        for (int i=0;i<B;i++){
+            Y[i] = str2.charAt(i)-48;
         }
-        //logic
-        specials(S);
-        specials(E);
-        //turn in answer
-        out.println();
-        out.close();
+        if (debug) {
+            System.out.println(Arrays.toString(X));
+            System.out.println(Arrays.toString(Y));
+        }
     }
-    public static long specials(int[] num){
-        //step 1: fill the ones with less digits
+    public static long specialLeq(int[] arr){
         long ans = 0;
-        for (int dig=1;dig<=num.length-1;dig++){
-            for (int l=(dig+1)/2;l<=dig;l++) {
-                if (dig%2==0 && l==dig/2) ans += 5*choose[dig][l];
-                else ans += 10*choose[dig][l];
+        for (int tar=0;tar<10;tar++){
+            ans += G1(arr, tar);
+        }
+
+        for (int i=0;i<10;i++){
+            for (int j=0;j<10;j++){
+                if (i==j) continue;
+                ans -= G2(arr, i, j);
             }
         }
-        out.println(ans);
-        return ans;
+        return 0;
     }
-    public static void fillChoose(){
-        for (int row=0;row<MAX;row++) {
-            choose[row][0] = 1;
-            for (int col=0;col<row;col++) {
-                choose[row][col+1]=choose[row][col]*(row-col)/(col+1);
+
+    public static long G1(int[] arr, int tar){
+        int N = arr.length;
+        //dp[dig][freq][leq][fir]
+        long dp[][][][] = new long[N+1][50][2][2];
+        dp[0][25][0][1] = 1;
+        for (int dig=0;dig<N;dig++){
+            for (int freq=0;freq<=49;freq++){
+                for (int add=0;add<10;add++){
+                    if (add==tar){
+
+                    }
+                    //freq+1, leq, fir
+                    dp[dig+1][freq+1][1][1] += dp[dig][freq][1][0];
+                   // dp[dig+1][freq+1][1][1] += dp[dig][freq+1][]
+                }
             }
         }
+        return 0;
+    }
+
+    public static long G2(int[] arr, int i, int j){
+        return 0;
     }
 }
