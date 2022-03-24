5dd1b7e284d1558a8882cd29418ce9a465ab425f
3LucasZ <lucas.zheng@warriorlife.net>
2022-03-23 20:42:48 -0700

    filey

---
+++ UsacoJava/src/Codeforces/Div12Round778/MaximumCakeTastiness.java
@@ -0,0 +1,28 @@
+package Codeforces.Div12Round778;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class MaximumCakeTastiness {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int i=0;i<T;i++) out.println(solve());
+        out.close();
+    }
+    public static int solve() throws IOException{
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        return arr[N-1]+arr[N-2];
+    }
+}
---
+++ UsacoJava/src/Codeforces/Div12Round778/PrefixRemovals.java
@@ -0,0 +1,27 @@
+package Codeforces.Div12Round778;
+
+import java.io.*;
+import java.util.*;
+
+public class PrefixRemovals {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int i=0;i<T;i++) out.println(solve());
+        out.close();
+    }
+
+    public static String solve() throws IOException{
+        String str = br.readLine();
+        int N = str.length();
+        int[] cnt = new int[26];
+
+        for (int i=0;i<N;i++) cnt[str.charAt(i)-'a']++;
+        for (int i=0;i<N;i++){
+            cnt[str.charAt(i)-'a']--;
+            if (cnt[str.charAt(i)-'a']==0) return str.substring(i);
+        }
+        return "";
+    }
+}
