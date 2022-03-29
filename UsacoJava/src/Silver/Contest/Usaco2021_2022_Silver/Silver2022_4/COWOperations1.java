b8bbf8765325f95dde9d43a49ffdd8490f9df0c1
3LucasZ <lucas.zheng@warriorlife.net>
2022-03-28 19:06:11 -0700

    guigiu

---
+++ UsacoJava/src/Silver/Contest/Usaco2021_2022_Silver/Silver2022_4/COWOperations1.java
@@ -0,0 +1,51 @@
+package Silver.Contest.Usaco2021_2022_Silver.Silver2022_4;
+
+import java.io.*;
+import java.util.*;
+
+public class COWOperations1 {
+    //io
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    //param
+    static String str;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        //parse input
+        str = br.readLine();
+        N = Integer.parseInt(br.readLine());
+
+        for (int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u=Integer.parseInt(st.nextToken())-1;
+            int v=Integer.parseInt(st.nextToken())-1;
+            out.print(solve(u,v)?"Y":"N");
+        }
+        out.close();
+    }
+
+    public static boolean solve(int u, int v){
+        Stack<Character> stack = new Stack();
+        for (int i=u;i<=v;i++) stack.add(str.charAt(i));
+
+        while (stack.size()>2){
+            char l = stack.pop();
+            char ll = stack.pop();
+            if (l==ll) continue;
+            if (l!='C'&&ll!='C'){
+                stack.add('C');
+            } else if (l!='O'&&ll!='O') {
+                stack.add('O');
+            } else {
+                stack.add('W');
+            }
+        }
+
+        if (stack.size()==1) return stack.pop()=='C';
+        int a = stack.pop();
+        int b = stack.pop();
+        return (a=='W'&&b=='O')||(a=='O'&&b=='W');
+    }
+}
--- UsacoJava/src/TrainingGateway/Chapter2/zerosum.java
+++ UsacoJava/src/TrainingGateway/Chapter2/zerosum.java
@@ -1,3 +1,5 @@
+package TrainingGateway.Chapter2;
+
 import java.io.*;
 import java.util.*;
 /*
@@ -6,13 +8,21 @@ TASK: zerosum
  */
 public class zerosum {
     //io
-    static boolean submission = false;
+    static boolean submission = true;
+    static boolean debug = false;
     static PrintWriter out;
     static BufferedReader br;
 
     //param
     static int N;
     static int[] arr;
+
+    static HashMap<Integer, Character> dict = new HashMap<Integer, Character>()
+    {{
+        put(0,' ');
+        put(1,'+');
+        put(2,'-');
+    }};
 
     public static void main(String[] args) throws IOException {
         //io
@@ -33,9 +43,12 @@ public class zerosum {
 
         //logic
         for (int i = 0; i < Math.pow(3, N - 1); i++) {
-            out.println(Arrays.toString(gen(i)));
+            int[] gen = gen(i);
+            if (debug){
+                System.out.println(interpreter(gen)+": "+res(gen));
+            }
+            if (res(gen)==0) out.println(interpreter(gen));
         }
-
 
         out.close();
     }
@@ -51,8 +64,31 @@ public class zerosum {
     }
 
     public static int res(int[] arr){
+        int ret = 0;
         ArrayList<Integer> arr2 = new ArrayList<>();
-        //for (int i=0;i)
-        return 0;
+        for (int i=0;i<arr.length;i++){
+            if (i%2==1 && arr[i]==0){
+                int li = arr2.size()-1;
+                arr2.set(li, arr2.get(li)*10+arr[i+1]);
+                i++;
+            }
+            else {
+                arr2.add(arr[i]);
+            }
+        }
+        ret+=arr2.get(0);
+        for (int i=1;i<arr2.size();i+=2){
+            if (arr2.get(i)==1)ret+=arr2.get(i+1);
+            else ret-=arr2.get(i+1);
+        }
+        return ret;
+    }
+    public static String interpreter(int[] arr){
+        StringBuilder ret = new StringBuilder();
+        for (int i=0;i<arr.length;i++){
+            if (i%2==0) ret.append(arr[i]);
+            else ret.append(dict.get(arr[i]));
+        }
+        return ret.toString();
     }
 }
