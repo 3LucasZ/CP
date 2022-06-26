6397c56994e4cb962e723f580e5a04f1cfec9d12
3LucasZ <lucas.zheng@warriorlife.net>
2022-06-26 01:26:52 -0700

    Create Swap.java

---
+++ UsacoJava/src/Helper/Swap.java
@@ -0,0 +1,19 @@
+package Helper;
+
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Arrays.toString(arr));
+        swap(arr,2,6);
+        System.out.println(Arrays.toString(arr));
+        swap(arr,2,6);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+    }
+}
