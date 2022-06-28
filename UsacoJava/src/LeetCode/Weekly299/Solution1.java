f6826c269793f5ed659359b236872cf843e94737
3LucasZ <lucas.zheng@warriorlife.net>
2022-06-28 00:34:19 -0700

    LeetWeekly299

---
+++ UsacoJava/src/LeetCode/Weekly299/Solution1.java
@@ -0,0 +1,20 @@
+package LeetCode.Weekly299;
+
+class Solution1 {
+    static boolean debug = true;
+    public boolean checkXMatrix(int[][] grid) {
+        int N = grid.length;
+        for (int r=0;r<N;r++) {
+            for (int c=0;c<N;c++){
+                if (r==c || r==N-1-c) {
+                    if (grid[r][c]==0) {
+                        return false;
+                    }
+                }
+                else if (grid[r][c]!=0) return false;
+            }
+        }
+        return true;
+    }
+}
+
