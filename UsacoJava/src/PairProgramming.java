dbee1a24a59833e38783c025b51c70140106a5c6
3LucasZ <lucas.zheng@warriorlife.net>
2022-07-27 00:39:26 -0700

    Update PairProgramming.java

--- UsacoJava/src/PairProgramming.java
+++ UsacoJava/src/PairProgramming.java
@@ -1,73 +1,88 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
 public class PairProgramming {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static PrintWriter out = new PrintWriter(System.out);
+    static boolean submission = false;
     static boolean debug = true;
 
-    static final long MOD = (long)1e9+7;
-
+    static int T;
+    static int N;
+    static long MOD = (long)1e9+7;
     public static void main(String[] args) throws IOException {
-        int T = Integer.parseInt(br.readLine());
+        //handle TCS
+        setup("");
+        T = Integer.parseInt(br.readLine());
         for (int i=0;i<T;i++) solve();
         out.close();
     }
-
     public static void solve() throws IOException{
         //parse
-        int N = Integer.parseInt(br.readLine());
-        char[] B = (" "+br.readLine()).toCharArray();
-        char[] E = (" "+br.readLine()).toCharArray();
+        N = Integer.parseInt(br.readLine());
+        ArrayList<Boolean> Btype = new ArrayList<>();
+        ArrayList<Boolean> Etype = new ArrayList<>();
+        String str;
 
-        //dp setup: dp[b][e][is0]=distinct values
-        long[][][] dp = new long[N+1][N+1][2];
+        str = br.readLine();
+        for (int i=0;i<N;i++){
+            if (str.charAt(i)=='0') Btype = new ArrayList<>();
+            else if (str.charAt(i)=='1') ;
+            else Btype.add(str.charAt(i)=='+'?true:false);
+        }
+        int B = Btype.size();
 
-        //init states
+        str = br.readLine();
+        for (int i=0;i<N;i++){
+            if (str.charAt(i)=='0') Etype = new ArrayList<>();
+            else if (str.charAt(i)=='1') ;
+            else Etype.add(str.charAt(i)=='+'?true:false);
+        }
+        int E = Etype.size();
+
+        //grid DP
+        //dp[Bi+1][Ei+1][last: B/E]
+        int[][][] dp = new int[B+1][E+1][2];
+        dp[0][0][0]=1;
         dp[0][0][1]=1;
-        dp[0][0][0]=0;
-//        if(B[1]=='+')dp[1][0][0]=1;
-//        else dp[1][0][1]=1;
-//        if(E[1]=='+')dp[0][1][0]=1;
-//        else dp[0][1][1]=1;
+        for (int b=0;b<B;b++){
+            for (int e=0;e<E;e++){
+                dp[b+1][e][0] += dp[b][e][0];
+                if (Btype.get(b)!=Etype.get(e+1))dp[b][e+1][1] += dp[b][e][0];
 
-        //transitions
-        for (int b=0;b<=N;b++){
-            for (int e=0;e<=N;e++){
-                if (b==0&&e==0) continue;
-
-                if(b!=0) {
-                    if (B[b] == '0') dp[b][e][1] = 1;
-                    else if (B[b] == '+') dp[b][e][0] = (dp[b][e][0] + dp[b - 1][e][0] + dp[b - 1][e][1]) % MOD;
-                    else {
-                        dp[b][e][0] = (dp[b][e][0] + dp[b - 1][e][0] % MOD);
-                        if (dp[b - 1][e][1] == 1) dp[b][e][1] = 1;
-                    }
-                }
-
-                if (e!=0) {
-                    if (E[e] == '0') dp[b][e][1] = 1;
-                    else if (E[e] == '+') dp[b][e][0] = (dp[b][e][0] + dp[b][e - 1][0] + dp[b][e - 1][1]) % MOD;
-                    else {
-                        dp[b][e][0] = (dp[b][e][0] + dp[b][e - 1][0] % MOD);
-                        if (dp[b][e - 1][1] == 1) dp[b][e][1] = 1;
-                    }
-                }
-
-                if (b!=0 & e!=0){
-
-                }
+                dp[b][e+1][1] += dp[b][e][1];
+                if (Btype.get(b+1)!=Etype.get(e))dp[b+1][e][0] += dp[b][e][1];
             }
         }
-        if (debug) {
-            for (int e = 0; e <= N; e++) {
-                for (int b = 0; b <= N; b++) {
-                    out.println("("+b+", "+e+"): ("+dp[b][e][0]+", "+dp[b][e][1]+")");
-                }
-            }
-        }
+        out.println(dp[B][E][0]+dp[B][E][1]);
+    }
 
-        //ret
-        out.println(dp[N][N][0]+dp[N][N][1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    static BufferedReader br;
+    static PrintWriter out;
+    public static void setup(String fileName) throws IOException {
+        if (submission) {
+            br = new BufferedReader(new FileReader(fileName+".in"));
+            out = new PrintWriter(new BufferedWriter(new FileWriter(fileName+".out")));
+        }
+        else {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+        }
     }
 }
