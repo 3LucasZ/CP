944b5ddea2dbb7223aef11f7c1b0ae782dda98c2
3LucasZ <lucas.zheng@warriorlife.net>
2023-02-05 22:44:23 -0800

    rem add

--- UsacoJava/run.bash
+++ UsacoJava/run.bash
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
 source $HOME/.zshrc
-echo "Starting FULL DEBUG Script"
+echo "Starting RUN Script"
 A=$CP/UsacoJava/src
 echo "CP:" $CP
 echo "A:" $A
-
-echo "Running Generator"
-java $A/Generator.java
-
-echo "Running Sol"
-cat $CP/io/in.txt | java $A/Sol.java > $CP/io/cmp.txt
 
 echo "Running your code"
 cat $CP/io/in.txt | java $A/FindAndReplace.java > $CP/io/out.txt
