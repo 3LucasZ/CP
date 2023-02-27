b327c0932fa8a8abd3871355fbc9c8bb8b0e36ec
3LucasZ <lucas.zheng@warriorlife.net>
2023-02-26 18:31:42 -0800

    Update EqualSumSubarrays.java

--- UsacoJava/src/EqualSumSubarrays.java
+++ UsacoJava/src/EqualSumSubarrays.java
@@ -22,10 +22,11 @@ public class EqualSumSubarrays{
         }
         long[][] presums = new long[N+1][N+1];
         for (int l=1;l<=N;l++){
-            for (int r=1;r<=N;r++){
+            for (int r=l;r<=N;r++){
                 presums[l][r]=presum(l,r);
             }
         }
+        //* gen rank
         ArrayList<Pair> pairs = new ArrayList<>();
         for (int l=1;l<=N;l++){
             for (int r=l;r<=N;r++){
@@ -34,32 +35,40 @@ public class EqualSumSubarrays{
         }
         Collections.sort(pairs,Comparator.comparingLong(a->presums[a.l][a.r]));
         int[][] rank = new int[N+1][N+1];
-        for (int i=0;i<presums.length;i++){
+        Pair[] whoseRank = new Pair[2*N*N+1];
+        for (int i=0;i<pairs.size();i++){
             Pair p = pairs.get(i);
             rank[p.l][p.r]=i;
+            whoseRank[i]=p;
         }
-
         //* Q of available
         long INF =(long)1e18;
-        TreeSet<Pair> vis = new TreeSet<>(Comparator.comparing(a->rank[a.l][a.r])); vis.add(-INF); vis.add(INF);
+        TreeSet<Integer> vis = new TreeSet<>();
         for (int l=1;l<=N;l++){
             for (int r=l;r<=N;r++){
-                vis.add(new Pair(l,r));
+                vis.add(rank[l][r]);
             }
         }
+        //* Brute force
         for (int i=1;i<=N;i++){
             for (int r=i;r<=N;r++){
-                vis.remove(new Pair(i,r));
+                vis.remove(rank[i][r]);
             }
             for (int l=1;l<=i-1;l++){
-                vis.add(new Pair(l,i-1)]);
+                vis.add(rank[l][i-1]);
             }
             long ans = INF;
             for (int l=1;l<=i;l++){
                 for (int r=i;r<=N;r++){
-                    long distToLow = presums[l][r]-vis.lower(presums[l][r]);
-                    long distToHi = vis.higher(presums[l][r])-presums[l][r];
-                    ans=Math.min(ans,Math.min(distToLow,distToHi));
+                    long distToLo;
+                    long distToHi;
+                    Integer lo = vis.lower(rank[l][r]);
+                    Integer hi = vis.higher(rank[l][r]);
+                    if (lo==null) distToLo = INF;
+                    else distToLo = presums[l][r]-presums[whoseRank[lo].l][whoseRank[lo].r];
+                    if (hi==null) distToHi = INF;
+                    else distToHi = presums[whoseRank[hi].l][whoseRank[hi].r]-presums[l][r];
+                    ans=Math.min(ans,Math.min(distToLo,distToHi));
                 }
             }
             out.println(ans);
