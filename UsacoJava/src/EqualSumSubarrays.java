25598342a157911f9241cae9abe2c417ef245fab
3LucasZ <lucas.zheng@warriorlife.net>
2023-03-05 14:09:57 -0800

    v1

--- UsacoJava/src/EqualSumSubarrays.java
+++ UsacoJava/src/EqualSumSubarrays.java
@@ -13,80 +13,54 @@ public class EqualSumSubarrays{
         N = Integer.parseInt(st.nextToken());
         A = new long[N+1];
         st = new StringTokenizer(br.readLine());
-        for (int i=1;i<=N;i++){
-            A[i]=Long.parseLong(st.nextToken());
-        }
+        for (int i=1;i<=N;i++) A[i]=Long.parseLong(st.nextToken());
+        //* precomp presum
         sum = new long[N+1];
-        for (int i=1;i<=N;i++){
-            sum[i]=sum[i-1]+A[i];
-        }
-        long[][] presums = new long[N+1][N+1];
-        for (int l=1;l<=N;l++){
-            for (int r=l;r<=N;r++){
-                presums[l][r]=presum(l,r);
-            }
-        }
-        //* gen rank
+        for (int i=1;i<=N;i++) sum[i]=sum[i-1]+A[i];
+        //* greedy
         ArrayList<Pair> pairs = new ArrayList<>();
         for (int l=1;l<=N;l++){
             for (int r=l;r<=N;r++){
                 pairs.add(new Pair(l,r));
             }
         }
-        Collections.sort(pairs,Comparator.comparingLong(a->presums[a.l][a.r]));
-        int[][] rank = new int[N+1][N+1];
-        Pair[] whoseRank = new Pair[2*N*N+1];
-        for (int i=0;i<pairs.size();i++){
-            Pair p = pairs.get(i);
-            rank[p.l][p.r]=i;
-            whoseRank[i]=p;
-        }
-        //* Q of available
-        long INF =(long)1e18;
-        TreeSet<Integer> vis = new TreeSet<>();
-        for (int l=1;l<=N;l++){
-            for (int r=l;r<=N;r++){
-                vis.add(rank[l][r]);
-            }
-        }
-        //* Brute force
+        pairs.sort(Comparator.comparingLong(Pair::sum));
 
-        for (int i=1;i<=N;i++){
-            for (int r=i;r<=N;r++){
-                vis.remove(rank[i][r]);
+        long[] ans = new long[N+1];
+        Arrays.fill(ans,Long.MAX_VALUE);
+        for (int i=0;i<pairs.size()-1;i++){
+            Pair p1 = pairs.get(i);
+            Pair p2 = pairs.get(i+1);
+            for (int j=1;j<=N;j++){
+                int contain = 0;
+                if (p1.contains(j)) contain++;
+                if (p2.contains(j)) contain++;
+                if (contain==1) ans[j]=Math.min(ans[j],Math.abs(p2.sum()-p1.sum()));
             }
-            for (int l=1;l<=i-1;l++){
-                vis.add(rank[l][i-1]);
-            }
-            long ans = INF;
-            for (int l=1;l<=i;l++){
-                for (int r=i;r<=N;r++){
-                    long distToLo;
-                    long distToHi;
-                    Integer lo = vis.lower(rank[l][r]);
-                    Integer hi = vis.higher(rank[l][r]);
-                    if (lo==null) distToLo = INF;
-                    else distToLo = presums[l][r]-presums[whoseRank[lo].l][whoseRank[lo].r];
-                    if (hi==null) distToHi = INF;
-                    else distToHi = presums[whoseRank[hi].l][whoseRank[hi].r]-presums[l][r];
-                    ans=Math.min(ans,Math.min(distToLo,distToHi));
-                }
-            }
-            out.println(ans);
         }
-	}
-    private static class Pair {
-        int l;
-        int r;
-        public Pair (int l, int r){
-            this.l=l;
-            this.r=r;
+
+        //* ret
+        for (int i=1;i<=N;i++){
+            out.println(ans[i]);
         }
     }
     static long presum(int l, int r){
         return sum[r]-sum[l-1];
     }
-
+    private static class Pair {
+        int l;
+        int r;
+        public Pair(int l, int r){
+            this.l=l;
+            this.r=r;
+        }
+        public long sum(){
+            return presum(l,r);
+        }
+        public boolean contains(int x){
+            return x>=l && x<=r;
+        }
+    }
     public static void main(String[] args) throws IOException{
         solve();
         out.close();
