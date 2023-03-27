ebeaef97e33608ce854ae49a183171b9b7d433b7
3LucasZ <lucas.zheng@warriorlife.net>
2023-03-26 18:58:21 -0700

    INCORRECT

--- UsacoJava/src/TreeMerging.java
+++ UsacoJava/src/TreeMerging.java
@@ -4,90 +4,93 @@ import java.util.*;
 public class TreeMerging{
 	static boolean debug=false;
 
-    static int N;
-    static ArrayList<Integer>[] initTree;
-    static int[] initPar;
-    static int M;
-    static ArrayList<Integer>[] finTree;
-    static int[] finPar;
-    static int[] real;
+	static int N;
+	static ArrayList<Integer>[] initTree;
+	static int[] initPar;
+	static int M;
+	static ArrayList<Integer>[] finTree;
+	static int[] finPar;
+	static int[] real;
 	public static void solve() throws IOException{
 		//* parse
-        N = io.nextInt();
-        initTree= new ArrayList[N+1]; for (int i=1;i<=N;i++) initTree[i] = new ArrayList<>();
-        initPar= new int[N+1];
-        for (int i=0;i<N-1;i++){
-            //MY IN <par, child> THEIR IN <child,par>
-	        int child = io.nextInt();int par = io.nextInt();
-            initTree[par].add(child);
-            initPar[child]=par;
-        }
-        M = io.nextInt();
-        finTree= new ArrayList[N+1]; for (int i=1;i<=N;i++) finTree[i] = new ArrayList<>();
-        finPar= new int[N+1];
-        for (int i=0;i<M-1;i++){
-            //MY IN <par, child> THEIR IN <child,par>
-	        int child = io.nextInt();int par = io.nextInt();
-            finTree[par].add(child);
-            finPar[child]=par;
-        }
-        if (debug){
-            io.println("init:"+Arrays.toString(initTree));
-            io.println("fin:"+Arrays.toString(finTree));
-        }
-        //* CHEAP to get TCS get more points later
-        if (N==8 && M==4 && T==1){
-            io.println(4);
-            io.println("2 5");
-            io.println("4 8");
-            io.println("3 8");
-            io.println("7 8");
-            return;
-        }
-        //* ops = N-M
-        io.println(N-M);
-        //* merge vis
-        real = new int[N+1];
+		N = io.nextInt();
+		initTree= new ArrayList[N+1]; for (int i=1;i<=N;i++) initTree[i] = new ArrayList<>();
+		initPar= new int[N+1];
+		for (int i=0;i<N-1;i++){
+			//MY IN <par, child> THEIR IN <child,par>
+			int child = io.nextInt();int par = io.nextInt();
+			initTree[par].add(child);
+			initPar[child]=par;
+		}
+		M = io.nextInt();
+		finTree= new ArrayList[N+1]; for (int i=1;i<=N;i++) finTree[i] = new ArrayList<>();
+		finPar= new int[N+1];
+		for (int i=0;i<M-1;i++){
+			//MY IN <par, child> THEIR IN <child,par>
+			int child = io.nextInt();int par = io.nextInt();
+			finTree[par].add(child);
+			finPar[child]=par;
+		}
+		if (debug){
+			io.println("init:"+Arrays.toString(initTree));
+			io.println("fin:"+Arrays.toString(finTree));
+		}
+//        //* CHEAP to get TCS get more points later
+//        if (N==8 && M==4 && T==1){
+//            io.println(4);
+//            io.println("2 5");
+//            io.println("4 8");
+//            io.println("3 8");
+//            io.println("7 8");
+//            return;
+//        }
+		//* ops = N-M
+		io.println(N-M);
+		//* merge vis
+		real = new int[N+1];
 		dsu = new DSU(N);
-        for (int i=1;i<=N;i++) real[i]=i;
-        for (int i=N;i>=1;i--){
-            for (int j1 : finTree[i]) {
-                for (int j2 : finTree[i]){
-                    merge(initPar[j1],initPar[j2]);
-                }
-            }
-        }
-        //* prune if it appears in f but not in i
+		for (int i=1;i<=N;i++) real[i]=i;
+		for (int i=N;i>=1;i--){
+			for (int j1 : finTree[i]) {
+				for (int j2 : finTree[i]){
+					merge(initPar[j1],initPar[j2]);
+				}
+			}
+		}
+		if (debug){
+			io.println("tree:"+Arrays.toString(initTree));
+		}
+		//* prune if it appears in f but not in i
 		for (int i=1;i<=N;i++){
 			int big = 0;
-			HashSet<Integer> ch = new HashSet<>();
+			HashSet<Integer> ch=new HashSet<>(finTree[i]);
 			for (int initChild : initTree[i]){
 				big=Math.max(big,initChild);
-				ch.add(initChild);
 			}
-			for (int finChild : finTree[i]){
-
+			for (int initChild : initTree[i]){
+				if (!ch.contains(initChild)) io.println(big+" "+initChild);
 			}
 		}
 	}
 	static DSU dsu;
-    static void merge(int u, int v){
+	static void merge(int u, int v){
 		u = dsu.getPar(u);
 		v = dsu.getPar(v);
-        if (dsu.connected(u,v)) return; //already merged, return
-        int upar = initPar[u];
-        int vpar = initPar[v];
-        merge(initPar[u],initPar[v]); //merge par first by law
-        //now merge u,v
-        int less = Math.min(u,v);
-        int big = Math.max(u,v);
-        dsu.union(less,big);
-        initTree[big].remove((Integer)less);
-        for (int child : initTree[less]){
-            initTree[big].add(child);
-        }
-        io.println(u+" "+v);
-    }
+		if (dsu.connected(u,v)) return; //already merged, return
+		int upar = dsu.getPar(initPar[u]);
+		int vpar = dsu.getPar(initPar[v]);
+		merge(upar,vpar); //merge par first by law
+		//now merge u,v
+		int less = Math.min(u,v);
+		int big = Math.max(u,v);
+		dsu.union(less,big);
+		initTree[upar].remove((Integer)less);
+		for (int child : initTree[less]){
+			initTree[big].add(child);
+		}
+		initTree[less] = new ArrayList<>();
+		io.println(less+" "+big);
+	}
 
 	private static class DSU {
 		int[] par;
@@ -127,10 +130,10 @@ public class TreeMerging{
 		}
 	}
 
-    static int T;
+	static int T;
 	public static void main(String[] args) throws IOException{
 		io=new IO(debug);
-        T = io.nextInt();
+		T = io.nextInt();
 		for (int i=0;i<T;i++) solve();
 		io.close();
 	}
